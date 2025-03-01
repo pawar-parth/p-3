4b4d922c901ccfceac8bbeb70900dd2f1eb4659d
pawar-parth <pawarparth233@gmail.com>
2025-03-01 15:40:32 +0530

    Delete gemini.py

--- gemini.py
+++
@@ -1,7 +0,0 @@
-from google import genai
-
-client = genai.Client(api_key="AIzaSyBUUaVyyOdTS25VtZf_vOM4O3TMINOv5ys")
-response = client.models.generate_content(
-    model="gemini-1.5-flash", contents=input("Enter prompt :")
-)
-print(response.text)
